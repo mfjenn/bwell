55fccf92690b3e0d97bd762eeba710739eb9f72b
Mary Jenn <mary@maryfjenn.com>
2013-12-19 13:44:42 -0800

    debugging for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -9,27 +9,27 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130910210350) do
+ActiveRecord::Schema.define(version: 20130910210350) do
 
-  create_table "inquiries", :force => true do |t|
+  create_table "inquiries", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "subject"
     t.text     "message"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "profiles", :force => true do |t|
+  create_table "profiles", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address1"
@@ -39,22 +39,22 @@ ActiveRecord::Schema.define(:version => 20130910210350) do
     t.integer  "zip"
     t.string   "email"
     t.string   "comments"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "replies", :force => true do |t|
+  create_table "replies", force: true do |t|
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "first_name"
     t.string   "last_name"
   end
